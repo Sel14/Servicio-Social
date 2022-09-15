f62efd080646981ecb098437d37bc852505b7792
Sel14 <35499172+Sel14@users.noreply.github.com>
2022-09-14 21:54:01 -0500

    Frond comentado
    Comentarios para en frond

--- aprendizajeAdaptativo/src/app/componentes/crear-pregunta/crear-pregunta.component.ts
+++ aprendizajeAdaptativo/src/app/componentes/crear-pregunta/crear-pregunta.component.ts
@@ -11,6 +11,7 @@ export class CrearPreguntaComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder) { }
 
+  //Se declara el formGroup para guardar los datos
   examen!: FormGroup;
 
   //Parte para selecionar unidad y tema
@@ -26,7 +27,7 @@ export class CrearPreguntaComponent implements OnInit {
   }
   //roytuts.com/cascading-or-dependent-dropdown-using-angular/
 
-
+  //Se inicializa la pagina generando un formulario vacio
   ngOnInit(): void {
     this.examen = new FormGroup({
       reactivos: new FormArray([
@@ -35,71 +36,81 @@ export class CrearPreguntaComponent implements OnInit {
     })
   }
 
-
+  //Se construye el formulario para los reactivos
   crearReactivo(): FormGroup {
     return this.formBuilder.group({
       contenido: "",
+      procedimiento: "",
       nivel: "",
-      ra: "",
-      preguntaCritica: "",
+      correcto: "",
+      unidadId: "",
+      temaId: "",
       respuestas: new FormArray([
         this.crearRespuestas()
-      ])
+      ]),
+      
     })
   }
 
+  //boton para crear un reactivo
   agregarReactivo() {
     const reactivos = this.examen.get('reactivos') as FormArray
     reactivos.push(this.crearReactivo());
   }
 
+  //se utiliza para obtener la posicion del reactivo
   getReactivo(form: any) {
     return form.controls.reactivos.controls
   }
 
 
-  //aqui se genera la seccion para generar respuesta no se que es lo que falla
-
+  //Se construye el formulario para los reactivos
   crearRespuestas(): FormGroup {
     return this.formBuilder.group({
       contenido: "",
-      correcto: "",
-      preguntaSiguiente: "",
     })
   }
 
+  //boton para crear una nueva respuesta
   agregarRespuesta(k: any) {
     const respuesta = (this.examen.get('reactivos') as FormArray).controls[k].get('respuestas') as FormArray;
     respuesta.push(this.crearRespuestas());
   }
 
+  //se obtine la posicion de la respuesta dentro del reactivo
   getRespuesta(form: any) {
     return form.controls.respuestas.controls
 
   }
 
-
-
+  //Creo que no se utiliza más xd
   eliminarUnidad(i: any) {
     const unidad = this.examen.get('unidades') as FormArray;
     unidad.removeAt(i);
   }
 
+  
+  //Creo que no se utiliza más xd
   eliminarTema(i: any, j: any) {
     const tema = (this.examen.get('unidades') as FormArray).controls[i].get('temas') as FormArray;
     tema.removeAt(j);
   }
 
-
+  //Este si xd, es oara eliminar un reattivo
   eliminarReactivo(i: any) {
     const reactivos = this.examen.get('reactivos') as FormArray;
     reactivos.removeAt(i);
   }
 
+  //Se usa para eliminar una respuesta
   eliminarRespuesta(i: any, j: any) {
     const respuesta = (this.examen.get('reactivos') as FormArray).controls[i].get('respuestas') as FormArray;
     respuesta.removeAt(j);
   }
 
+  //Aqui es para mandar para la base de dato que no esta implementada xd
+  mandar(){
+    console.log(this.examen.value)
+  }
 
 }
--- aprendizajeAdaptativo/src/app/componentes/inicio/inicio.component.ts
+++ aprendizajeAdaptativo/src/app/componentes/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap'; 
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal,
+    private formBuilder: FormBuilder
+    ) { }
 
   ngOnInit(): void {
+  }
+
+  //para mostrar los datos
+  usuario: any = "Sebastian Echeverria"
+  materias: Array<any> = [
+    {nombre: 'Geometria Analitica', id: 'GA01'},
+    {nombre: 'Algebra Lineal', id: 'AL01'}
+  ]
+  closeResult = ''
+  
+  //Para el formulario
+  materia = this.formBuilder.group({
+    nombre: ['']
+  })
+
+
+  guardar(){
+    console.log(this.materia.value)
+  }
+
+  //cosas para el modal
+  open(content: any) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title', size: 'lg' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 }
